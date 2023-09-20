b62515e56a1cb5b8b27fdb6883018281ffbc0dfb
Abderrahmane369 <cszamasu@gmail.com>
2023-09-21 00:27:40 +0100

    OK

--- A/push.c
+++ A/push.c
@@ -7,29 +7,28 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    int data;
-    stack_t *ptr;
-    char **args;
-    if (line_number == 0)
-    {
-        fprintf(stderr, "Error: Invalid line number\n");
-        exit(EXIT_FAILURE);
-    }
-    args = tokenize(*arr, " \t");
-    data = atoi(args[1]);
-    printf("(%d)\n", data);
-    ptr = malloc(sizeof(stack_t));
-    if (ptr == NULL)
-    {
-        fprintf(stderr, "L%u: Error: malloc failed\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    ptr->prev = *stack;
-    ptr->n = data;
-    ptr->next = NULL;
-
-    if (*stack)
-        (*stack)->next = ptr;
-    else
-        *stack = ptr;
+int data;
+stack_t *ptr;
+char **args;
+if (line_number == 0)
+{
+fprintf(stderr, "Error: Invalid line number\n");
+exit(EXIT_FAILURE);
+}
+args = tokenize(*arr, " \t");
+data = atoi(args[1]);
+ptr = malloc(sizeof(stack_t));
+if (ptr == NULL)
+{
+fprintf(stderr, "L%u: Error: malloc failed\n", line_number);
+exit(EXIT_FAILURE);
+}
+ptr->prev = NULL;
+ptr->n = data;
+ptr->next = (*stack);
+if (*stack != NULL)
+{
+(*stack)->prev = ptr;
+}
+*stack = ptr;
 }
