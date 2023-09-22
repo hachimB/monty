3d945362cf01db549e1b8e4f25ff1afc60b9b39f
Abderrahmane369 <cszamasu@gmail.com>
2023-09-22 03:57:55 +0100

    0

--- main.c
+++ main.c
@@ -7,39 +7,42 @@
  */
 
 char **args;
+
 int main(int argc, char **argv)
 {
-    FILE *_byteCode_;
-    char *_codeLine_ = NULL;
-    stack_t *__Stack__;
-    instruction_t _instr[7] = {{"pop", pop}, {"swap", swap}, {"add", add}, {"push", push}, {"pint", pint}, {"pall", pall}, {"nop", nop}};
-    size_t l = 0;
-    ssize_t read;
-    unsigned int line = 1;
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    __Stack__ = NULL;
-    _byteCode_ = fopen(argv[1], "r");
-    if (!_byteCode_)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    while ((read = _getline_(&_codeLine_, &l, _byteCode_)) != -1)
-    {
-        _read_(
-            &args,
-            &_codeLine_,
-            &line,
-            _instr,
-            &_byteCode_,
-            &__Stack__);
-    }
-    freeStack(__Stack__), free(_codeLine_);
-    _codeLine_ = NULL;
-    fclose(_byteCode_);
-    return (0);
+	FILE *_byteCode_;
+	char *_codeLine_ = NULL;
+	stack_t *__Stack__;
+	instruction_t _instr[7] = {{"pop", pop}, {"swap", swap}, {"add", add},
+		{"push", push}, {"pint", pint}, {"pall", pall}, {"nop", nop}};
+	size_t l = 0;
+	ssize_t read;
+	unsigned int line = 1;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	__Stack__ = NULL;
+	_byteCode_ = fopen(argv[1], "r");
+	if (!_byteCode_)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((read = _getline_(&_codeLine_, &l, _byteCode_)) != -1)
+	{
+		_read_(
+				&args,
+				&_codeLine_,
+				&line,
+				_instr,
+				&_byteCode_,
+				&__Stack__);
+	}
+	freeStack(__Stack__), free(_codeLine_);
+	_codeLine_ = NULL;
+	fclose(_byteCode_);
+	return (0);
 }
